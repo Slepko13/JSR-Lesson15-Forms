71970da1e473c8cba7baa92035abd1b23b08ecdc
Mykola Slepko <slepko13@gmail.com>
2020-05-18 17:46:40 +0300

    Updates

---
+++ precache-manifest.75b5b25f2397bf87321125ab3a374b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ce6ef437894a04d31689ada292cbd5",
+    "url": "/JSR-Lesson15-Forms/index.html"
+  },
+  {
+    "revision": "bcf6bde9cc8b9c66bb22",
+    "url": "/JSR-Lesson15-Forms/static/css/main.653c485c.chunk.css"
+  },
+  {
+    "revision": "9ce577387be028e619f9",
+    "url": "/JSR-Lesson15-Forms/static/js/2.b8f53627.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/JSR-Lesson15-Forms/static/js/2.b8f53627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf6bde9cc8b9c66bb22",
+    "url": "/JSR-Lesson15-Forms/static/js/main.f2dd1a9f.chunk.js"
+  },
+  {
+    "revision": "451e2eee1ddb91dd8fde",
+    "url": "/JSR-Lesson15-Forms/static/js/runtime-main.51b7f4ef.js"
+  }
+]);
